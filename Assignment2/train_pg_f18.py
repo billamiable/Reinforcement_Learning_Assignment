44c97f6f11891dbe6592689d24e069212a0f3ae9
yujie-wang <510552624@qq.com>
2018-09-21 13:59:48 +0800

    remote push

--- Assignment2/train_pg_f18.py
+++ Assignment2/train_pg_f18.py
@@ -96,6 +96,7 @@ class Agent(object):
 
     def init_tf_sess(self):
         tf_config = tf.ConfigProto(inter_op_parallelism_threads=1, intra_op_parallelism_threads=1) 
+        tf_config.gpu_options.allow_growth = True
         self.sess = tf.Session(config=tf_config)
         self.sess.__enter__() # equivalent to `with self.sess:`
         tf.global_variables_initializer().run() #pylint: disable=E1101
