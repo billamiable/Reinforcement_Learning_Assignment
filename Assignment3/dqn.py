fixed tf precision error

Assignment3/dqn.py
@@ -178,7 +178,6 @@ class QLearner(object):
         dropout = False
     q_t   = q_func(obs_t_float, self.num_actions, scope='q_func', reuse=False, 
                        dropout=dropout, keep_prob=self.keep_per)
-    self.q_test = q_t
     q_tp1 = q_func(obs_tp1_float, self.num_actions, scope='target_q_func_vars', reuse=False, 
                        dropout=dropout, keep_prob=self.keep_per)
 
@@ -220,14 +219,11 @@ class QLearner(object):
                                                      depth=self.num_actions, 
                                                      on_value=1.0, off_value=0.0), axis=1)
     else:
+        # Soft maximum
         if self.explore == 'soft_q':
             print('using soft q learning')
-            # print(q_tp1)  
-            # print(tf.exp(q_tp1))
-            # print(tf.reduce_sum(tf.exp(q_tp1),1))
-            q_tp1_max = tf.log( tf.reduce_sum(tf.exp(q_tp1),1) )
-            # print(q_tp1_max)
-            # exit()
+            # q_tp1_max = tf.log( tf.reduce_sum(tf.exp(q_tp1),1) )
+            q_tp1_max = tf.reduce_logsumexp(q_tp1)
         else:
             q_tp1_max = tf.reduce_max(q_tp1, 1)
         
@@ -373,18 +369,16 @@ class QLearner(object):
             action = np.random.randint(0, self.num_actions)
         else:
             recent_obs = self.replay_buffer.encode_recent_observation()
-            q_d, q_t = self.session.run([self.q_dist, self.q_test], feed_dict={self.obs_t_ph: [recent_obs], 
+            q_d = self.session.run(self.q_dist, feed_dict={self.obs_t_ph: [recent_obs], 
                                                            self.Temp: self.exploration.value(self.t),
                                                            self.keep_per: 1.0})
-            # action = np.random.choice(q_d[0], p=q_d[0])
-            # action = np.argmax(q_d[0] == action)
-            # print('loss',t_loss)
-            print('qt',q_t)
-            print('qd',q_d)
+            if 0:
+                print('in',input_q)
+                print('qt',q_t)
+                print('qd',q_d)
             
             action = np.random.choice(self.num_actions, p=q_d[0])
-            # print(action)
-            # exit()
+
     if self.explore == 'bayesian':
         # print("using bayesian exploration!")
         if (not self.model_initialized):