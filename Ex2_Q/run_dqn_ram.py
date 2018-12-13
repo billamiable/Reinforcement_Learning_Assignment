f30d800360007def692958dd944bd09e5538dbaf
Yu Chieh Lan <yclan2@berkeley.edu>
2018-12-12 23:38:58 -0800

    fix indent and temperature settings

--- Ex2_Q/run_dqn_ram.py
+++ Ex2_Q/run_dqn_ram.py
@@ -58,7 +58,7 @@ def atari_learn(env,
 
     exploration_schedule = PiecewiseSchedule(
         [
-            (0, 0.2),
+            (0, 10),
             (1e6, 0.1),
             (num_iterations / 2, 0.01),
         ], outside_value=0.01
